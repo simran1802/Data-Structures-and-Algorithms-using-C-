175cc86b77595062eff1f2b697589ee5e85a010e
Simran <51385869+simran1802@users.noreply.github.com>
2020-07-01 16:01:34 +0530

    Create bubble_sort.cpp

---
+++ bubble_sort.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+void swap(int *x,int* y){
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+void bubble(int a[],int n){
+    int i,j,flag=0;
+    for(i=0;i<n-1;i++){
+        flag=0;
+        for(j=0;j<n-i-1;j++){
+            if(a[j]>a[j+1]){
+                swap(&a[j],&a[j+1]);
+                flag=1;
+            }
+        }
+        if(flag==0)
+            break;
+    }
+}
+int main(){
+
+        int a[ ]={2,8,6,9,3,0,65535};
+
+        int n=sizeof(a)/sizeof(a[0]);
+
+        bubble(a, n);
+
+        for(int i=0;i<n;i++)
+
+            cout<<a[i]<<"\t";
+
+        cout<<endl;
+
+        return 0;
+
+}
